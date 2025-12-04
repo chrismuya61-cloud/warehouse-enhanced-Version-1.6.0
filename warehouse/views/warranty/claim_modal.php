160b7c8fa4563cbb19f45ac48e55513d12d0a86d
chrismuya61-cloud <chrismuya61@gmail.com>
2025-12-04 17:55:00 +0300

    Update claim_modal.php

--- warehouse/views/warranty/claim_modal.php
+++ warehouse/views/warranty/claim_modal.php
@@ -1,5 +1,4 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
-
 <div class="modal fade" id="warranty_claim_modal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -7,24 +6,19 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title"><?php echo _l('file_claim'); ?></h4>
             </div>
-            
             <?php echo form_open(admin_url('warehouse/add_edit_claim'), ['id' => 'warranty-claim-form']); ?>
             <div class="modal-body">
-                
                 <input type="hidden" name="detail_id" value="<?php echo $detail_id; ?>">
                 <input type="hidden" name="commodity_id" value="<?php echo $commodity_id; ?>">
                 <input type="hidden" name="customer_id" value="<?php echo $customer_id; ?>">
-
                 <div class="row">
                     <div class="col-md-12">
                         <div class="alert alert-info">
-                            <?php echo _l('commodity_name'); ?>: <strong><?php echo isset($item) ? $item->description : ''; ?></strong>
+                            <p><?php echo _l('commodity_name'); ?>: <strong><?php echo isset($item) ? $item->description : ''; ?></strong></p>
+                            <p><?php echo _l('serial_number'); ?>: <strong><?php echo isset($serial_number) ? $serial_number : 'N/A'; ?></strong></p>
                         </div>
-
                         <?php echo render_date_input('claim_date', 'claim_date', date('Y-m-d')); ?>
-
                         <?php echo render_textarea('issue_description', 'issue_description', ''); ?>
-                        
                         <div class="form-group">
                             <label for="status"><?php echo _l('claim_status'); ?></label>
                             <select name="status" id="status" class="selectpicker" data-width="100%">
@@ -37,7 +31,6 @@
                     </div>
                 </div>
             </div>
-            
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal"><?php echo _l('close'); ?></button>
                 <button type="submit" class="btn btn-info"><?php echo _l('submit'); ?></button>
